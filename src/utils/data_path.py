85aa560d2410686a990bc37e72f5b5526ffa5e69
Paul-Andre Henegar <paul-andre.henegar@mail.mcgill.ca>
2022-10-17 15:11:45 -0400

    move stuff, create pyproject.toml

--- src/utils/data_path.py
+++ src/utils/data_path.py
@@ -6,9 +6,9 @@ if "DATA_PATH" in os.environ:
     DATA_PATH = os.environ["DATA_PATH"]
 else:
     _SCRIPT_DIR = os.path.dirname(__file__)
-    DATA_PATH = os.path.abspath(os.path.join(_SCRIPT_DIR, "../data/"))
+    DATA_PATH = os.path.abspath(os.path.join(_SCRIPT_DIR, "../../data/"))
     # Canary in case I move files around and forget to change the path
     assert os.path.isdir(DATA_PATH)
     assert os.path.isdir(DATA_PATH + "/original")
-    assert os.path.isdir(DATA_PATH + "/interim")
+    # assert os.path.isdir(DATA_PATH + "/interim")
     assert os.path.isdir(DATA_PATH + "/database")
